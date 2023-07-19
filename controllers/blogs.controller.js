8c77725e6e6158939b0109e0c53f06967628ce95
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-07-18 23:17:52 -0600

    ultimo blog

--- controllers/blogs.controller.js
+++ controllers/blogs.controller.js
@@ -1,80 +1,93 @@
 import Blog from "../models/blog.model.js";
 import mongoose from "mongoose";
 
-// METODO GET - Obtener todos los blogs con paginación
+// METODO GET - Obtener todos los blogs con paginación y filtros de consulta
 export const getBlogs = async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-
-    const totalDocs = await Blog.countDocuments();
-    const totalPages = Math.ceil(totalDocs / limit);
-
-    const blogs = await Blog.find()
-      .skip((page - 1) * limit)
-      .limit(limit)
-      .sort({ fecha: -1 });
-
-    res.json({
-      data: blogs,
-      currentPage: page,
-      totalPages: totalPages,
-    });
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-
-// METODO GET - Obtener un blog por ID o título
-export const getBlog = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    // Verificar si el parámetro es un ID válido de MongoDB
-    const isMongoId = mongoose.Types.ObjectId.isValid(id);
-
-    let blog;
-    if (isMongoId) {
-      blog = await Blog.findById(id);
-    } else {
-      blog = await Blog.findOne({ titulo: id });
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+  
+      // Filtros de consulta (opcional)
+      const { titulo, categoria, fecha } = req.query;
+      const query = {};
+  
+      if (titulo) {
+        query.titulo = { $regex: new RegExp(titulo, "i") };
+      }
+  
+      if (categoria) {
+        query.categoria = categoria;
+      }
+  
+      if (fecha) {
+        query.fecha = new Date(fecha);
+      }
+  
+      // Calcular el número total de documentos
+      const totalDocs = await Blog.countDocuments(query);
+      const totalPages = Math.ceil(totalDocs / limit);
+  
+      // Consulta con paginación y filtros
+      const blogs = await Blog.find(query)
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .sort({ fecha: -1 });
+  
+      res.json({
+        data: blogs,
+        currentPage: page,
+        totalPages: totalPages,
+      });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
     }
-
-    if (!blog) {
-      return res.status(404).json({ message: "Blog no encontrado" });
+  };
+  
+  // METODO GET - Obtener un blog por ID o título
+  export const getBlog = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      // Verificar si el parámetro es un ID válido de MongoDB
+      const isMongoId = mongoose.Types.ObjectId.isValid(id);
+  
+      let blog;
+      if (isMongoId) {
+        blog = await Blog.findById(id);
+      } else {
+        blog = await Blog.findOne({ titulo: id });
+      }
+  
+      if (!blog) {
+        return res.status(404).json({ message: "Blog no encontrado" });
+      }
+  
+      res.json(blog);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
     }
-
-    res.json(blog);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-
-// METODO POST - Crear un nuevo blog
-export const createBlog = async (req, res) => {
-  try {
-    const {
-      titulo,
-      cuerpo_blog,
-      fecha,
-      imagen_destacada,
-      categoria,
-    } = req.body;
-
-    const blog = new Blog({
-      titulo,
-      cuerpo_blog,
-      fecha,
-      imagen_destacada,
-      categoria,
-    });
-
-    await blog.save();
-    res.status(201).json(blog);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
+  };
+  
+  // METODO POST - Crear un nuevo blog
+  export const createBlog = async (req, res) => {
+    try {
+      const { titulo, cuerpo_blog, fecha, imagen_destacada, categoria } = req.body;
+  
+      const blog = new Blog({
+        titulo,
+        cuerpo_blog,
+        fecha: new Date(fecha),
+        imagen_destacada,
+        categoria,
+      });
+  
+      await blog.save();
+      res.json(blog);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  };
+  
 
 // METODO PUT - Actualizar un blog por ID
 export const updateBlog = async (req, res) => {
