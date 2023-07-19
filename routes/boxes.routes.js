3b3e7bca6a7398309105aedeb7c16844936fce6d
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-07-18 19:23:56 -0600

    ubucaicon carta

--- routes/boxes.routes.js
+++ routes/boxes.routes.js
@@ -5,6 +5,7 @@ import {
   updateBox,
   deleteBox,
   getBox,
+  getUbicacionCarta, // Importa la función getUbicacionCarta
 } from '../controllers/boxes.controllerse.js';
 
 const routerBoxes = Router();
@@ -14,5 +15,7 @@ routerBoxes.post('/boxes', createBox);
 routerBoxes.put('/boxes/:id', updateBox);
 routerBoxes.delete('/boxes/:id', deleteBox);
 routerBoxes.get('/boxes/:id', getBox);
+routerBoxes.get('/boxes/:id/ubicacion', getUbicacionCarta); // Agrega la ruta para obtener la ubicación de una carta
+
 
 export default routerBoxes;
