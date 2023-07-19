c70625cfd3b1418b43a77829271d147b839e7421
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-07-18 23:08:41 -0600

    arreglar blogs

--- routes/blogs.routes.js
+++ routes/blogs.routes.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
 import {
-  getBlogs,
-  getBlog,
-  createBlog,
-  updateBlog,
-  deleteBlog,
-} from "../controllers/blogs.controller.js";
+    getBlogs,
+    getBlog,
+    createBlog,
+    updateBlog,
+    deleteBlog,
+  } from '../controllers/blogs.controller.js';
 
 const router = Router();
 
-router.get("/blogs", getBlogs);
-router.post("/blogs", createBlog);
-router.put("/blogs/:id", updateBlog);
-router.delete("/blogs/:id", deleteBlog);
-router.get("/blogs/:id", getBlog); // Ruta para obtener un blog por ID o título
+router.get('/blogs', getBlogs);
+router.post('/blogs', createBlog);
+router.put('/blogs/:id', updateBlog);
+router.delete('/blogs/:id', deleteBlog);
+router.get('/blogs/:id', getBlog); // Ruta para obtener un blog por ID o título
 
 export default router;
  
