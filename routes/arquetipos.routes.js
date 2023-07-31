81daea42c420fff65a631337de6b01b3c8f453f5
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-07-30 18:12:36 -0600

    se repara routes arquetipos

--- routes/arquetipos.routes.js
+++ routes/arquetipos.routes.js
@@ -1,3 +1,5 @@
+// routes/arquetipos.routes.js
+import { Router } from 'express';
 import {
     getArquetipos,
     createArquetipo,
@@ -6,15 +8,15 @@ import {
     getArquetipo,
     getArquetipoByName, // no olvides importar esto
   } from '../controllers/arquetipos.controller.js';
-  
-  const routerArquetipos = Router();
-  
-  routerArquetipos.get('/arquetipos', getArquetipos);
-  routerArquetipos.post('/arquetipos', createArquetipo);
-  routerArquetipos.put('/arquetipos/:id', updateArquetipo);
-  routerArquetipos.delete('/arquetipos/:id', deleteArquetipo);
-  routerArquetipos.get('/arquetipos/:id', getArquetipo);
-  routerArquetipos.get('/arquetipos/nombre/:nombre_arquetipo', getArquetipoByName); // agrega esta linea
-  
-  export default routerArquetipos;
-  
+
+const routerArquetipos = Router();
+
+routerArquetipos.get('/arquetipos', getArquetipos);
+routerArquetipos.post('/arquetipos', createArquetipo);
+routerArquetipos.put('/arquetipos/:id', updateArquetipo);
+routerArquetipos.delete('/arquetipos/:id', deleteArquetipo);
+routerArquetipos.get('/arquetipos/:id', getArquetipo);
+routerArquetipos.get('/arquetipos/nombre/:nombre_arquetipo', getArquetipoByName); // agrega esta linea
+
+
+export default routerArquetipos;
