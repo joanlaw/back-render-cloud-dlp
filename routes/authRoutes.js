28f5eea044a4af4fb2b2d2791ec1626b1d9d00db
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-12 22:06:51 -0600

    agregando .js para routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, callback } from '../controllers/authController';
+import { login, logout, callback } from '../controllers/authController.js';
 
 const router = express.Router();
 
