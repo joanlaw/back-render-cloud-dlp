b1cf5e466a635ef95f99591ba1fe9e1f8a023293
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-13 15:31:59 -0600

    dfdklfj

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const passport = require('passport');
-const { login, logout, callback, getUserImage } = require('../controllers/authController.js');
+// authRoutes.js
+import express from 'express';
+import { login, logout, callback, getUserImage } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.get('/login', (req, res, next) => {
-    console.log('Iniciando la autenticación'); // Verificar inicio de autenticación
-    login(req, res, next);
+  console.log('Iniciando la autenticación');
+  login(req, res, next);
 });
+
 router.get('/logout', logout);
 router.get('/callback', (req, res, next) => {
-    console.log('Callback de autenticación', req.query); // Verificar datos del callback
-    callback(req, res, next);
+  console.log('Callback de autenticación', req.query);
+  callback(req, res, next);
 });
+
 router.get("/get-user-image", getUserImage);
 
-module.exports = router;
+export default router; // Asegúrate de exportar el router
