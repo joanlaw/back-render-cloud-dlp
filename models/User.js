7f73a5ab3c43e097e6880a91135cfa0b553c1e4b
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-13 00:50:40 -0600

    dfd users

--- models/User.js
+++ models/User.js
@@ -14,6 +14,6 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
   // ...
-}, { timestamps: true });
+}, { timestamps: true, collection: 'users' }); // Aquí especificas el nombre de la colección
 
 export default mongoose.model('User', userSchema);
