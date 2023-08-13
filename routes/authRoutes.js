5ea60ca1981cf85e77d01adfa2991703d1edf8ab
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-12 21:30:00 -0600

    authcontroller discord actualizado

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import authController from '../controllers/authController.js';
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
