2e951cb1fac45e7baf573709d9d1ccc1c249596c
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-21 01:41:31 -0600

    subida de imagenes

--- routes/leagues.routes.js
+++ routes/leagues.routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer'; // Importa multer
 import {
     getLeagues,
     createLeague,
@@ -11,13 +12,23 @@ import {
   } from '../controllers/leagues.controllers.js';
 
 const leaguesRouter = Router();
+// Configuración de multer para la subida de imágenes
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, './uploads/'); // Directorio donde se guardarán las imágenes
+  },
+  filename: function (req, file, cb) {
+      cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname); // Nombre único para el archivo
+  }
+});
+const upload = multer({ storage: storage });
 
 leaguesRouter.get('/leagues', getLeagues);
 leaguesRouter.get('/leagues/:id', getLeagueById); // agrega esta línea
 leaguesRouter.get('/leagues/discordId/:discordId', getTournamentsByDiscordId);
 leaguesRouter.get('/leagues/organizer/:discordId', getLeaguesByOrganizer);
 
-leaguesRouter.post('/leagues', createLeague);
+leaguesRouter.post('/leagues', upload.single('file'), createLeague); // Utiliza el middleware "upload.single('file')" para manejar la subida de imágenes
 leaguesRouter.post('/leagues/:leagueId/enroll', enrollPlayer);  // ¡Añade esta línea!
 leaguesRouter.delete('/leagues/:id', deleteLeague);
 
