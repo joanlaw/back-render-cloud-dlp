ce8a6e05bb7597c849cf457095e83dfbd9a279bb
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-25 22:17:13 -0600

    lkdlfk

--- models/ChatRoom.model.js
+++ models/ChatRoom.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const chatRoomSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
   },
   participants: [{
     type: mongoose.Schema.Types.ObjectId,
