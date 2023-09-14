fdeab8342382238a75cc0a77891af1edd5d0f566
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-09-14 15:50:33 -0600

    lkdlfk

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(cors({
 
 app.use(morgan('dev'));
 
-app.use(express.json());
+app.use(express.json({ limit: '3mb' })); // Establece el límite a 3MB
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
