6cb19ab732a9fbff775eba17395b174a9be45c68
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-09-25 20:19:00 -0600

    update cards images cards

--- routes/rush.route.js
+++ routes/rush.route.js
@@ -12,13 +12,18 @@ import {
 
 const router = Router();
 
+import fileUpload from 'express-fileupload'
+
 router.get('/rushes', getRushes);
 //router.get('/rushes', getRushByParams);
 router.post('/rushes', createRush);
 /*router.post('/rushes', fileUpload({
   useTempFiles: true, tempFileDir: './uploads'
 }), createRush); */
-router.put('/rushes/:id', updateRush);
+router.put('/rushes/:id', fileUpload({
+  useTempFiles: true,
+  tempFileDir: './uploads'
+}), updateRush);
 router.delete('/rushes/:id', deleteRush);
 //router.get('/rushes/:id', getRushById);
 router.get('/rushes/:value', getRushByValue);
